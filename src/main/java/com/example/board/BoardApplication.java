ffd786cb0080f65f054c79cf16ea057e61d5358a
yujirou <yujirou@example.com>
2023-07-17 15:59:48 +0900

    AWSの更新

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -3,11 +3,17 @@ package com.example.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BoardApplication {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
+public class BoardApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BoardApplication.class);
+	}
 }
